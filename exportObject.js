bf4ecc684f55199eaf0657c2b3cf71831e8f2688
tapasnag <tapas.nag76@gmail.com>
2022-12-06 18:02:39 +0000

    import and export of an object solution

---
+++ exportObject.js
@@ -0,0 +1,6 @@
+module.export = class sampleClass {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+};
---
+++ importObject.js
@@ -0,0 +1,3 @@
+const sampleClass = require("./exportObject.js");
+const sample = new sampleClass("John", "Wilson");
+console.log(sampleClass);
